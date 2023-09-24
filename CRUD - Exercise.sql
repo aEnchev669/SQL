32e9467f3a8b43ab2b474595b76318651a05a860
aEnchev669 <aenchev669@abv.bg>
2023-09-24 22:13:28 +0300

    Create CRUD - Exercise.sql

---
+++ CRUD - Exercise.sql
@@ -0,0 +1,42 @@
+Go
+USE [SoftUni]
+--Problem 02
+SELECT * FROM [Departments]
+
+
+--Problem 03
+SELECT [Name] FROM [Departments]
+
+--Problem 04
+SELECT [FirstName], [LastName], [Salary] FROM [Employees]
+
+--Problem 05
+SELECT [FirstName], [MiddleName], [LastName] FROM [Employees]
+
+--Problem 06
+SELECT CONCAT ([FirstName], '.', [LastName] , '@' , 'softuni.bg')
+	AS [Full Email Address]
+	FROM [Employees]
+
+--Problem 07
+SELECT DISTINCT [Salary] FROM [Employees]
+
+--Problem 08
+SELECT *  FROM [Employees]
+	WHERE [JobTitle] = 'Sales Representative'
+
+--Problem 09
+SELECT [FirstName], [LastName], [JobTitle]  FROM [Employees]
+	WHERE [Salary] >= 20000 AND [Salary] <= 30000
+
+--Problem 10
+SELECT CONCAT ( [FirstName], ' ', [MiddleName], ' ', [LastName])
+	AS [Full Name]
+	FROM [Employees]
+	WHERE [Salary] = 25000 OR [Salary] = 14000 OR [Salary] = 12500 OR [Salary] = 23600
+
+--Problem 11
+SELECT [FirstName], [LastName] FROM [Employees]
+	WHERE [ManagerID] IS NULL
+
+SELECT * FROM [Employees]
