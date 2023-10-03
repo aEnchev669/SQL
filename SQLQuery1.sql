b4563a27c90fa8b86a3f37333d411b19f969e1e0
aEnchev669 <aenchev669@abv.bg>
2023-10-03 23:41:43 +0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,15 @@
-SELECT * FROM Employees WHERE [FirstName] = 'Kevin'
-ORDER BY LastName, Salary  DESC
+SELECT [FirstName], [LastName], [Salary] FROM [Employees]
+	WHERE [Salary] > 50000
+	ORDER BY [Salary] DESC
+
+SELECT TOP 5 [FirstName], [LastName] FROM Employees
+	ORDER BY [Salary] DESC
+
+
+SELECT [FirstName], [LastName] FROM Employees
+	WHERE [DepartmentID] != 4
+
+	SELECT * FROM [Employees]
+	ORDER BY [Salary] DESC, [FirstName] ASC, [LastName] DESC, [MiddleName] ASC
+
+SELECT * FROM [Employees]
