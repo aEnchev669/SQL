1710323aa250081acda942c27b8b72ecfa32f13e
aEnchev669 <aenchev669@abv.bg>
2023-10-14 11:53:57 +0300

    Delete 3.sql

--- Old exams/3.sql
+++
@@ -1,8 +0,0 @@
-USE master
-
-SELECT *  FROM [Invoices]
-WHERE [IssueDate] BETWEEN '2022-11-1' AND '2022-11-30'
-
-UPDATE Invoices SET DueDate = '2023-04-01' WHERE [IssueDate] BETWEEN '2022-11-1' AND '2022-11-30'
-UPDATE Clients SET AddressId = 3 WHERE [Name] LIKE '%CO%'
-
